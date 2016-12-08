5d2edada4564f00b3724b10d94d941bec06ee923
praneethgujarati <praneeth705@gmail.com>
2016-12-08 18:12:06 +0530

    Add files via upload

---
+++ PrintName.java
@@ -0,0 +1,7 @@
+class PrintName {
+	public static void main (String[] args)
+	{
+		// printf can be used w/o semi colon plus it gives options to format output. 
+ 		if (System.out.printf("Akhil Sharma").equals("")) {}
+	}
+}
